d06b713b2e6b35e1b8b463805573e114c617751b
Trần  Nguyễn Nhật Nam <tnnhatnam210@gmail.com>
2021-10-04 19:55:13 +0700

    edit interface

--- js/custom.js
+++ js/custom.js
@@ -190,27 +190,39 @@
 	   Slider Range
 	   ................................................. */
 
-	$(function() {
-		$("#slider-range").slider({
-			range: true,
-			min: 0,
-			max: 4000,
-			values: [1000, 3000],
-			slide: function(event, ui) {
-				$("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-			}
-		});
-		$("#amount").val("$" + $("#slider-range").slider("values", 0) +
-			" - $" + $("#slider-range").slider("values", 1));
-	});
+	// $(function() {
+	// 	$("#slider-range").slider({
+	// 		range: true,
+	// 		min: 0,
+	// 		max: 4000,
+	// 		values: [1000, 3000],
+	// 		slide: function(event, ui) {
+	// 			$("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+	// 		}
+	// 	});
+	// 	$("#amount").val("$" + $("#slider-range").slider("values", 0) +
+	// 		" - $" + $("#slider-range").slider("values", 1));
+	// });
 
 	/* ..............................................
 	   NiceScroll
 	   ................................................. */
 
-	$(".brand-box").niceScroll({
-		cursorcolor: "#9b9b9c",
-	});
+	// $(".brand-box").niceScroll({
+	// 	cursorcolor: "#9b9b9c",
+	// });
 	
 	
 }(jQuery));
+
+// var navItems = document.querySelectorAll('.nav-item')
+
+// // console.log(...navlinks)
+// Array.from(navItems).forEach(navItem => {
+// 	console.log(navItem)
+// 	navItem.onclick = () => {
+// 		document.querySelector('.nav-item.active').classList.remove('active')
+// 		// navItem.classList.add('active')
+// 		console.log(123)
+// 	}
+// })
